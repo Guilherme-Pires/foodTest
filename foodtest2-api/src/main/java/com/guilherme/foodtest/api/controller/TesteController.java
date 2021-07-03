f9fc1e271fd40427d8718c241fd97d8ec985ba36
Guilherme Pires Gonçalves <gui.pg@hotmail.com>
2021-07-03 15:54:53 -0300

    Commit atualização de aulas 03072021

---
+++ foodtest2-api/src/main/java/com/guilherme/foodtest/api/controller/TesteController.java
@@ -0,0 +1,27 @@
+package com.guilherme.foodtest.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.guilherme.foodtest.domain.model.Cozinha;
+import com.guilherme.foodtest.domain.repository.CozinhaRepository;
+
+@RestController
+@RequestMapping("teste")
+public class TesteController {
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	@GetMapping("/cozinhas/por-nome")
+	public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
+		return cozinhaRepository.consultarPorNome(nome);
+		
+	}
+	
+}
--- foodtest2-api/src/main/java/com/guilherme/foodtest/infrastructure/repository/CozinhaRepositoryImpl.java
+++ foodtest2-api/src/main/java/com/guilherme/foodtest/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -5,12 +5,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import com.guilherme.foodtest.domain.model.Cozinha;
 import com.guilherme.foodtest.domain.repository.CozinhaRepository;
 
-@Component
+@Repository
 public class CozinhaRepositoryImpl implements CozinhaRepository{
 
 	@PersistenceContext
@@ -24,6 +24,13 @@ public class CozinhaRepositoryImpl implements CozinhaRepository{
 	@Override
 	public Cozinha buscar(Long id) {
 		return manager.find(Cozinha.class, id);
+	}
+	
+	@Override
+	public List<Cozinha> consultarPorNome(String nome) {
+		return manager.createQuery("from Cozinha where nome = :nome", Cozinha.class)
+				.setParameter("nome", nome)
+				.getResultList();
 	}
 	
 	@Transactional
