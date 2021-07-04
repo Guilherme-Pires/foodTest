a97461c8e55f761ad2432ccadcf4dbd1ec287952
Guilherme Pires Gonçalves <gui.pg@hotmail.com>
2021-07-04 20:37:10 -0300

    Commit atualização aulas 04072021

--- foodtest2-api/src/main/java/com/guilherme/foodtest/api/controller/TesteController.java
+++ foodtest2-api/src/main/java/com/guilherme/foodtest/api/controller/TesteController.java
@@ -1,15 +1,18 @@
 package com.guilherme.foodtest.api.controller;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guilherme.foodtest.domain.model.Cozinha;
+import com.guilherme.foodtest.domain.model.Restaurante;
 import com.guilherme.foodtest.domain.repository.CozinhaRepository;
+import com.guilherme.foodtest.domain.repository.RestauranteRepository;
 
 @RestController
 @RequestMapping("/teste")
@@ -18,10 +21,48 @@ public class TesteController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-//	@GetMapping("/cozinhas/por-nome")
-//	public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
-//		return cozinhaRepository.consultarPorNome(nome);
-//		
-//	}
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+	
+	@GetMapping("/cozinhas/por-nome")
+	public List<Cozinha> cozinhasPorNome(String nome) {
+		return cozinhaRepository.findTodasByNomeContaining(nome);
+	}
+	
+	@GetMapping("/cozinhas/unica-por-nome")
+	public Optional<Cozinha> cozinhaPorNome(String nome) {
+		return cozinhaRepository.findByNome(nome);
+	}
+	
+	@GetMapping("/cozinhas/exists")
+	public boolean cozinhaExists(String nome) {
+		return cozinhaRepository.existsByNome(nome);
+	}
+	
+	@GetMapping("/restaurantes/por-taxa-frete")
+	public List<Restaurante> restaurantesPorTaxaFrete(
+			BigDecimal taxaInicial, BigDecimal taxaFinal) {
+		return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+	}
+	
+	@GetMapping("/restaurantes/por-nome-e-id")
+	public List<Restaurante> restaurantesPorNomeAndId(String nome, Long cozinhaId) {
+		return restauranteRepository.findTodasByNomeContainingAndCozinhaId(nome, cozinhaId);
+	}
+	
+	@GetMapping("/restaurantes/primeiro-por-nome")
+	public Optional<Restaurante> firstByNome(String nome) {
+		return restauranteRepository.findFirstByNomeContaining(nome);
+	} 
+	
+	@GetMapping("/restaurantes/primeiros2-por-nome")
+	public List<Restaurante> top2ByNome(String nome) {
+		return restauranteRepository.findTop2ByNomeContaining(nome);
+	}
+	
+	@GetMapping("/restaurantes/restaurantes-por-cozinhas")
+	public int countPorCozinha(Long cozinhaId) {
+		return restauranteRepository.countByCozinhaId(cozinhaId);
+	}
 	
 }
--- foodtest2-api/src/main/java/com/guilherme/foodtest/domain/repository/RestauranteRepository.java
+++ foodtest2-api/src/main/java/com/guilherme/foodtest/domain/repository/RestauranteRepository.java
@@ -1,5 +1,9 @@
 package com.guilherme.foodtest.domain.repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +12,14 @@ import com.guilherme.foodtest.domain.model.Restaurante;
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {	
 		
+	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+	
+	List<Restaurante> findTodasByNomeContainingAndCozinhaId(String nome, Long cozinha);
+	
+	Optional<Restaurante> findFirstByNomeContaining(String nome);
+	
+	List<Restaurante> findTop2ByNomeContaining(String nome);
+	
+	int countByCozinhaId(Long cozinha);
+	
 }
