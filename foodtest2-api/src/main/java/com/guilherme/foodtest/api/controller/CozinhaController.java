04f5b5670a88accd22c1807a4340dce15d3831d8
Guilherme Pires Gonçalves <gui.pg@hotmail.com>
2021-07-21 20:47:17 -0300

    Commit atualização de aulas 21072021
    8. Tratamento e modelagem de erros da API
    	8.3. Lançando exceções do tipo ResponseStatusException
    	8.4. Estendendo ResponseStatusException
    	8.5. Simplificando o código com o uso de @ResponseStatus em exceptions
    	8.6. Desafio: refatorando os serviços REST
    	8.7. Analisando os impactos da refatoração

--- foodtest2-api/src/main/java/com/guilherme/foodtest/api/controller/CozinhaController.java
+++ foodtest2-api/src/main/java/com/guilherme/foodtest/api/controller/CozinhaController.java
@@ -1,12 +1,10 @@
 package com.guilherme.foodtest.api.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import com.guilherme.foodtest.domain.exception.EntidadeEmUsoException;
-import com.guilherme.foodtest.domain.exception.EntidadeNaoEncontradaException;
+
 import com.guilherme.foodtest.domain.model.Cozinha;
 import com.guilherme.foodtest.domain.repository.CozinhaRepository;
 import com.guilherme.foodtest.domain.service.CadastroCozinhaService;
@@ -38,14 +35,8 @@ public class CozinhaController {
 	}
 	
 	@GetMapping("/{cozinhaId}")
-	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
-		Optional<Cozinha> cozinha = cozinhaRepository.findById(cozinhaId);
-		
-		if (cozinha.isPresent()) {
-			return ResponseEntity.ok(cozinha.get());
-		}
-		
-		return ResponseEntity.notFound().build();
+	public Cozinha buscar(@PathVariable Long cozinhaId) {
+		return cadastroCozinha.buscarOuFalhar(cozinhaId);
 	}
 	
 	@PostMapping
@@ -55,39 +46,19 @@ public class CozinhaController {
 	}
 	
 	@PutMapping("/{cozinhaId}")
-	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId,
+	public Cozinha atualizar(@PathVariable Long cozinhaId,
 			@RequestBody Cozinha cozinha) {
-		Optional<Cozinha> cozinhaAtual = cozinhaRepository.findById(cozinhaId);
+		Cozinha cozinhaAtual = cadastroCozinha.buscarOuFalhar(cozinhaId);
 		
-		if (cozinhaAtual.isPresent()) {
-			BeanUtils.copyProperties(cozinha, cozinhaAtual.get(), "id");
+			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
 			
-			Cozinha cozinhaSalva = cadastroCozinha.salvar(cozinhaAtual.get());
-			return ResponseEntity.ok(cozinhaSalva);
-		}
-		
-		return ResponseEntity.notFound().build();
+			return cadastroCozinha.salvar(cozinhaAtual);
 	}
-	
-//	@DeleteMapping("/{cozinhaId}")
-//	public ResponseEntity<?> remover(@PathVariable Long cozinhaId) {
-//		try {
-//			cadastroCozinha.excluir(cozinhaId);	
-//			return ResponseEntity.noContent().build();
-//			
-//		} catch (EntidadeNaoEncontradaException e) {
-//			return ResponseEntity.notFound().build();
-//			
-//		} catch (EntidadeEmUsoException e) {
-//			return ResponseEntity.status(HttpStatus.CONFLICT)
-//					.body(e.getMessage());
-//		}
-//	}
 	
 	@DeleteMapping("/{cozinhaId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long cozinhaId) {
-			cadastroCozinha.excluir(cozinhaId);	
+		cadastroCozinha.excluir(cozinhaId);			
 	}
 	
 }
--- foodtest2-api/src/main/java/com/guilherme/foodtest/domain/service/CadastroCidadeService.java
+++ foodtest2-api/src/main/java/com/guilherme/foodtest/domain/service/CadastroCidadeService.java
@@ -10,41 +10,50 @@ import com.guilherme.foodtest.domain.exception.EntidadeNaoEncontradaException;
 import com.guilherme.foodtest.domain.model.Cidade;
 import com.guilherme.foodtest.domain.model.Estado;
 import com.guilherme.foodtest.domain.repository.CidadeRepository;
-import com.guilherme.foodtest.domain.repository.EstadoRepository;
 
 @Service
 public class CadastroCidadeService {
 
-    @Autowired
-    private CidadeRepository cidadeRepository;
-    
-    @Autowired
-    private EstadoRepository estadoRepository;
-    
+	private static final String MSG_CIDADE_EM_USO 
+		= "Cidade de código %d não pode ser removida, pois está em uso";
+
+	private static final String MSG_CIDADE_NAO_ENCONTRADA 
+		= "Não existe um cadastro de cidade com código %d";
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private CadastroEstadoService cadastroEstado;
+	
 	public Cidade salvar(Cidade cidade) {
 		Long estadoId = cidade.getEstado().getId();
 
-		Estado estado = estadoRepository.findById(estadoId)
-			.orElseThrow(() -> new EntidadeNaoEncontradaException(
-					String.format("Não existe cadastro de estado com código %d", estadoId)));
+		Estado estado = cadastroEstado.buscarOuFalhar(estadoId);
 		
 		cidade.setEstado(estado);
 		
 		return cidadeRepository.save(cidade);
 	}
-    
-    public void excluir(Long cidadeId) {
+	
+	public void excluir(Long cidadeId) {
 		try {
 			cidadeRepository.deleteById(cidadeId);
 			
 		} catch (EmptyResultDataAccessException e) {
 			throw new EntidadeNaoEncontradaException(
-				String.format("Não existe um cadastro de cidade com código %d", cidadeId));
+				String.format(MSG_CIDADE_NAO_ENCONTRADA, cidadeId));
 		
 		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
-				String.format("Cidade de código %d não pode ser removida, pois está em uso", cidadeId));
+				String.format(MSG_CIDADE_EM_USO, cidadeId));
 		}
 	}
-    
+	
+	public Cidade buscarOuFalhar(Long cidadeId) {
+		return cidadeRepository.findById(cidadeId)
+			.orElseThrow(() -> new EntidadeNaoEncontradaException(
+					String.format(MSG_CIDADE_NAO_ENCONTRADA, cidadeId)));
+	}
+	
 }
