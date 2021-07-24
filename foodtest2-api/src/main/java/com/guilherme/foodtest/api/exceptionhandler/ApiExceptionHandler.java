7f5a74575fcd39c2f6bc57e32fdfae148b9e1e53
Guilherme Pires Gonçalves <gui.pg@hotmail.com>
2021-07-24 11:30:53 -0300

    Commit atualização aulad 24072021
    8. Tratamento e modelagem de erros da API
    	8.17. Conhecendo a RFC 7807 (Problem Details for HTTP APIs)
    	8.18. Padronizando o formato de problemas no corpo de respostas com a
    RFC 7807
    	8.19. Desafio: usando o formato de problemas no corpo de respostas
    	8.20. Customizando exception handlers de ResponseEntityExceptionHandler

--- foodtest2-api/src/main/java/com/guilherme/foodtest/api/exceptionhandler/ApiExceptionHandler.java
+++ foodtest2-api/src/main/java/com/guilherme/foodtest/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,10 +1,9 @@
 package com.guilherme.foodtest.api.exceptionhandler;
 
-import java.time.LocalDateTime;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -17,41 +16,88 @@ import com.guilherme.foodtest.domain.exception.NegocioException;
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	
-	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex, WebRequest request) {
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(
+			HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 		
-		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+		ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+		String detail = "Erro de sintaxe.";
+		
+		Problem problem = createProblemBuilder(status, problemType, detail)
+				.build();		
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+	}
+	
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<?> handleEntidadeNaoEncontradaException(
+			EntidadeNaoEncontradaException ex, WebRequest request) {
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		ProblemType problemType = ProblemType.ENTIDADE_NAO_ENCONTRADA;
+		String detail = ex.getMessage();
+		Problem problem = createProblemBuilder(status, problemType, detail)
+				.build();		
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
 	
 	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException ex, WebRequest request) {
-		
-		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
+	public ResponseEntity<?> handleEntidadeEmUsoException(
+	        EntidadeEmUsoException ex, WebRequest request) {
+	    
+	    HttpStatus status = HttpStatus.CONFLICT;
+	    ProblemType problemType = ProblemType.ENTIDADE_EM_USO;
+	    String detail = ex.getMessage();
+	    
+	    Problem problem = createProblemBuilder(status, problemType, detail)
+	    		.build();
+	    
+	    return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
 	
 	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarNegocioException(NegocioException ex, WebRequest request) {
-		
-		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+	public ResponseEntity<?> handleNegocioException(
+			NegocioException ex, WebRequest request) {
+
+	    HttpStatus status = HttpStatus.BAD_REQUEST;
+	    ProblemType problemType = ProblemType.ERRO_NEGOCIO;
+	    String detail = ex.getMessage();
+	    
+	    Problem problem = createProblemBuilder(status, problemType, detail)
+	    		.build();
+	    
+	    return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
 	
 	@Override
-	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
-			HttpStatus status, WebRequest request) {
+	protected ResponseEntity<Object> handleExceptionInternal(
+			Exception ex, Object body, HttpHeaders headers,
+			
+		HttpStatus status, WebRequest request) {
 		
 		if (body == null) {
-			body = Problema.builder()
-		            .dataHora(LocalDateTime.now())
-		            .mensagem(status.getReasonPhrase())
+			body = Problem.builder()
+					.title(status.getReasonPhrase())
+					.status(status.value())
 		            .build();
 		} else if (body instanceof String) {
-			body = Problema.builder()
-		            .dataHora(LocalDateTime.now())
-		            .mensagem((String) body)
+			body = Problem.builder()
+		            .title((String) body)
+		            .status(status.value())
 		            .build();
 		}
 				
 		return super.handleExceptionInternal(ex, body, headers, status, request);
+	}
+	
+	private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail) {
+					
+		return Problem.builder()
+			.status(status.value())
+			.type(problemType.getUri())
+			.title(problemType.getTitle())
+			.detail(detail);
 	}
 	
 }
---
+++ foodtest2-api/src/main/java/com/guilherme/foodtest/api/exceptionhandler/ProblemType.java
@@ -0,0 +1,22 @@
+package com.guilherme.foodtest.api.exceptionhandler;
+
+import lombok.Getter;
+
+@Getter
+public enum ProblemType {
+	
+	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensivel"),
+	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
+	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
+	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
+	
+	private String title;
+	private String uri;
+	
+	ProblemType(String path, String title) {
+		this.uri = "https://foodtest2.com.br" + path;
+		this.title = title;
+	}
+	
+	
+}
