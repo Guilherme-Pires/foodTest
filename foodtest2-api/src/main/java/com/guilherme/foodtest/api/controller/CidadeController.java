9d54ec9264e818e46487e6426f07868fe93f2545
Guilherme Pires Gonçalves <gui.pg@hotmail.com>
2021-07-23 21:10:12 -0300

    Commit atualização aulas 23072021
    8. Tratamento e modelagem de erros da API
    	8.12. Tratando exceções em nível de controlador com @ExceptionHandler
    	8.13. Tratando exceções globais com @ExceptionHandler e
    @ControllerAdvice
    	8.14. Desafio: implementando exception handler
    	8.15. Criando um exception handler global com
    ResponseEntityExceptionHandler
    	8.16. Customizando o corpo da resposta padrão de
    ResponseEntityExceptionHandler

--- foodtest2-api/src/main/java/com/guilherme/foodtest/api/controller/CidadeController.java
+++ foodtest2-api/src/main/java/com/guilherme/foodtest/api/controller/CidadeController.java
@@ -75,4 +75,6 @@ public class CidadeController {
 	    cadastroCidade.excluir(cidadeId);	
 	}
 	
+	
+	
 }
--- foodtest2-api/src/main/java/com/guilherme/foodtest/domain/exception/EntidadeEmUsoException.java
+++ foodtest2-api/src/main/java/com/guilherme/foodtest/domain/exception/EntidadeEmUsoException.java
@@ -1,15 +1,10 @@
 package com.guilherme.foodtest.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+public class EntidadeEmUsoException extends NegocioException {
 
-@ResponseStatus(value = HttpStatus.CONFLICT)
-public class EntidadeEmUsoException extends NegocioException{
+    private static final long serialVersionUID = 1L;
 
-	private static final long serialVersionUID = 1L;
-	
-	public EntidadeEmUsoException(String mensagem) {
-		super(mensagem);
-	}
-
-}
+    public EntidadeEmUsoException(String mensagem) {
+        super(mensagem);
+    }            
+}  
