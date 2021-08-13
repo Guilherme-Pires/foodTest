2da4ac1895dfb79db3c0c1f577be2d2319fae918
Guilherme Pires Gonçalves <gui.pg@hotmail.com>
2021-08-13 19:56:11 -0300

    Commit atualização aulas 13082021
    11. Boas práticas e técnicas para APIs
    	11.12. Refatorando e criando um assembler de DTO
    	11.13. Desafio: Refatorando e criando um disassembler do DTO
    	11.14. Adicionando e usando o ModelMapper

---
+++ foodtest2-api/src/main/java/com/guilherme/foodtest/api/RestauranteModelAssembler.java
@@ -0,0 +1,29 @@
+package com.guilherme.foodtest.api;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.guilherme.foodtest.api.model.RestauranteModel;
+import com.guilherme.foodtest.domain.model.Restaurante;
+
+@Component
+public class RestauranteModelAssembler {
+
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	public RestauranteModel toModel(Restaurante restaurante) {
+		return modelMapper.map(restaurante, RestauranteModel.class);
+	}
+	
+	public List<RestauranteModel> toCollectionModel(List<Restaurante> restaurantes) {
+		return restaurantes.stream()
+				.map(restaurante -> toModel(restaurante))
+				.collect(Collectors.toList());
+	}
+	
+}
