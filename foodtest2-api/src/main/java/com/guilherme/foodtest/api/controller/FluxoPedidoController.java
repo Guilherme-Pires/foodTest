ae8af0187dff5e041bc56fce8afddf51c5398eae
Guilherme Pires Gonçalves <gui.pg@hotmail.com>
2021-08-30 21:42:47 -0300

    Commit atualização aulas 30082021
    12. Modelagem avançada e implementação da API
    	12.23. Desafio: implementando endpoints de transição de status de
    pedidos
    	12.24. Refatorando o código de regras para transição de status de
    pedidos
    	12.25. Usando IDs vs UUIDs nas URIs de recursos

--- foodtest2-api/src/main/java/com/guilherme/foodtest/api/controller/FluxoPedidoController.java
+++ foodtest2-api/src/main/java/com/guilherme/foodtest/api/controller/FluxoPedidoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.guilherme.foodtest.domain.service.FluxoPedidoService;
 
 @RestController
-@RequestMapping(value = "/pedidos/{pedidoId}")
+@RequestMapping(value = "/pedidos/{codigoPedido}")
 public class FluxoPedidoController {
 
 	@Autowired
@@ -19,8 +19,20 @@ public class FluxoPedidoController {
 	
 	@PutMapping("/confirmacao")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void confirmar(@PathVariable Long pedidoId) {
-		fluxoPedido.confirmar(pedidoId);
+	public void confirmar(@PathVariable String codigoPedido) {
+		fluxoPedido.confirmar(codigoPedido);
+	}
+	
+	@PutMapping("/cancelamento")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void cancelar(@PathVariable String codigoPedido) {
+		fluxoPedido.cancelar(codigoPedido);
+	}
+	
+	@PutMapping("/entrega")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void entregar(@PathVariable String codigoPedido) {
+		fluxoPedido.entregar(codigoPedido);
 	}
 	
 }
--- foodtest2-api/src/main/java/com/guilherme/foodtest/domain/service/FluxoPedidoService.java
+++ foodtest2-api/src/main/java/com/guilherme/foodtest/domain/service/FluxoPedidoService.java
@@ -1,14 +1,10 @@
 package com.guilherme.foodtest.domain.service;
 
-import java.time.OffsetDateTime;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.guilherme.foodtest.domain.exception.NegocioException;
 import com.guilherme.foodtest.domain.model.Pedido;
-import com.guilherme.foodtest.domain.model.StatusPedido;
 
 @Service
 public class FluxoPedidoService {
@@ -17,18 +13,21 @@ public class FluxoPedidoService {
 	private EmissaoPedidoService emissaoPedido;
 	
 	@Transactional
-	public void confirmar(Long pedidoId) {
-		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
-		
-		if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
-			throw new NegocioException(
-					String.format("Status do pedido %d não pode ser alterado de %s para %s",
-							pedido.getId(), pedido.getStatus().getDescricao(), 
-							StatusPedido.CONFIRMADO.getDescricao()));
-		}
-		
-		pedido.setStatus(StatusPedido.CONFIRMADO);
-		pedido.setDataConfirmacao(OffsetDateTime.now());
+	public void confirmar(String codigoPedido) {
+		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
+		pedido.confirmar();
+	}
+	
+	@Transactional
+	public void cancelar(String codigoPedido) {
+		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
+		pedido.cancelar();
+	}
+	
+	@Transactional
+	public void entregar(String codigoPedido) {
+		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
+		pedido.entregar();
 	}
 	
 }
